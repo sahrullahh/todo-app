6090da1186ad901f648b59f92a30df84ea86b1f3
sahrullahh <necomcersnet@gmail.com>
2025-06-19 04:53:05 +0700

    Fix: final

--- vite.config.ts
+++ vite.config.ts
@@ -9,34 +9,6 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Tododay',
-        short_name: 'Tododay',
-        description: 'Aplikasi Todo List yang sederhana dan mudah digunakan',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
     }),
   ],
   resolve: {
