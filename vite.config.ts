1791028d22c6a5554b0d73735f8d81479bdec9a0
sahrullahh <necomcersnet@gmail.com>
2025-06-19 04:36:07 +0700

    Fix: build tsc vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
-
 export default defineConfig({
   plugins: [
     react(),
